5578f472e24ecca5e39d245c146209b7cac31e73
Brock Williams <brock@cottonwoodcomputer.com>
2014-08-27 11:29:08 -0600

    add scope to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   validates_presence_of :user, :post
+
+  scope :created_after, lambda { |d| where("created_at>?",d)}
+
+  def self.ransackable_scopes(auth=nil)
+    [:created_after]
+  end
 end
